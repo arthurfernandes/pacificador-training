e183dbb9964d9871af6b43fd0c791cc42e862de6
Arthur Fernandes <arthur.araujo@cds.eb.mil.br>
2016-02-29 13:09:54 -0300

    Issue #3 - Criação do modelo para os agentes, mapeamento das urls para a API Rest e esqueleto das funções responsáveis pela API

--- mysite/treinamento/models.py
+++ mysite/treinamento/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Agent(models.Model):
+    id = models.AutoField(primary_key=True,)
+    name = models.CharField(max_length=200,null=False)
+    lon = models.FloatField()
+    lat = models.FloatField()
--- mysite/treinamento/views.py
+++ mysite/treinamento/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 
 def index(request):
     return HttpResponseRedirect(reverse('treinamento:cenario'))
@@ -10,3 +11,42 @@ def cenario(request):
 
 def usuarios(request):
     return render(request,'treinamento/usuarios.html',{})
+
+def rest_agent(request,agent_id = None):
+    if agent_id == None:
+        #get all agents
+        if request.method == "GET":
+            return rest_agent_get_all(request)
+        #add agent
+        elif request.method == "POST":
+            return rest_agent_add(request)
+        #Method not allowed 405
+        else:
+            return HttpResponse(status=405)
+    elif request.method == "GET":
+        #get agent by id
+        return rest_agent_get(request,agent_id)
+    elif request.method == "PUT":
+        #update agent
+        return rest_agent_update(request,agent_id)
+    elif request.method == "DELETE":
+        #delete agent
+        return rest_agent_delete(request,agent_id)
+    else:
+        #Method not allowed
+        return HttpResponse(status=405)
+
+def rest_agent_get(request,agent_id):
+    return HttpResponse(status=405)
+
+def rest_agent_get_all(request):
+    return HttpResponse(status=405)
+
+def rest_agent_add(request):
+    return HttpResponse(status=405)
+
+def rest_agent_update(request,agent_id):
+    return HttpResponse(status=405)
+
+def rest_agent_delete(request,agent_id):
+    return HttpResponse(status=405)
